a2e7dcca36e62accaa083afa0a7035db9f8d6d46
xinrong.zhao <xinrong.zhao@sap.com>
2019-12-03 21:24:56 +0800

    use parcel

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .g-button {
+        height: var(--button-height);
+        font-size: var(--font-size);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+    }
+
+    .g-button:hover {
+        border-color: var(--border-color-hover);
+    }
+    
+    .g-button:active {
+        background-color: var(--button-active-bg);
+    }
+
+    .g-button:focus {
+        outline: none;
+    }
+</style>
