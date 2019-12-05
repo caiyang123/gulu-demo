68ca3c8a26222069b42261b8c6ab8330c9b5decc
xinrong.zhao <xinrong.zhao@sap.com>
2019-12-05 19:34:00 +0800

    add icon

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,31 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg class="icon" v-if="icon"><use :xlink:href="`#icon-${icon}`"></use></svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
-        
+        name: 'g-button',
+        props: {
+            'icon': {
+                type: String
+            }, 
+            'iconPosition': {
+                type: String,
+                default: 'left',
+                validator(value) {
+                    if (value != 'left' && value != 'right') {
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
+        }
     }
 </script>
 
@@ -16,17 +37,49 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-    }
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        padding: 0 .7em;
 
-    .g-button:hover {
-        border-color: var(--border-color-hover);
-    }
-    
-    .g-button:active {
-        background-color: var(--button-active-bg);
-    }
+        > .icon {
+            width: 1em;
+            height: 1em;
+            margin-top: 2px;
+        }
 
-    .g-button:focus {
-        outline: none;
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+
+            > .content {
+                order: 1;
+            }
+        }
     }
 </style>
