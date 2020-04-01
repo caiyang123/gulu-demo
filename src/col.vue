fbd007bbf4befe159ba2e4bfe1e5f9562a858c08
xinrong.zhao <xinrong.zhao@sap.com>
2020-04-01 19:59:00 +0800

    row col基本功能

---
+++ src/col.vue
@@ -0,0 +1,32 @@
+<template>
+    <div :class="['col', 'col-' + span]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GuluCol',
+        props: {
+            span: {
+                type: [Number, String]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .col {
+        height: 100px;
+        width: 50%;
+        background-color: grey;
+        border: 1px solid red;
+
+        $prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$prefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
